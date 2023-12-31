7cf49d8248c81b1e8d9682a20a742108c1682452
Kasra Rasaee <kasrarasaee@Kasras-MacBook-Pro-2.local>
2023-12-30 21:18:47 -0500

    updated github and docker build to include latest tag

--- docker_build.sh
+++ docker_build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 TAG=$1
+TAG_LATEST=$2
 
 conda_ism_core_path=$(find . -type f -name "alethic-ism-core*.tar.gz")
 conda_ism_core_path=$(basename $conda_ism_core_path)
@@ -9,7 +10,7 @@ conda_ism_db_path=$(find . -type f -name "alethic-ism-db*.tar.gz")
 conda_ism_db_path=$(basename $conda_ism_db_path)
 
 docker build \
- --progress=plain -t $TAG \
+ --progress=plain -t $TAG_LATEST -t $TAG \
  --build-arg CONDA_ISM_CORE_PATH=$conda_ism_core_path \
  --build-arg CONDA_ISM_DB_PATH=$conda_ism_db_path \
  --no-cache .
